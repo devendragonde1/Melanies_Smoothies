cf565f5ec5fcafe66f59b7f8980e39855194b809
devendragonde1 <devendra.gonde1@gmail.com>
2026-05-19 16:49:06 +0530

    Modify dataframe selection and display in Streamlit app
    Updated the data selection to include 'SEARCH_ON' column and enabled displaying the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 name_on_smoothie=st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ',name_on_smoothie)
-my_dataframe = session.table("smoothies.public.fruit_options").select((col('Fruit_name')))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select((col('Fruit_name'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list=st.multiselect(
 'Choose upto 5 ingredients:', my_dataframe, max_selections=5
 )
