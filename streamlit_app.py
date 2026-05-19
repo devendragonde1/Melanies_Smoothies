325dd2429b42fe8798ab54213a7ce52e04ee877e
devendragonde1 <devendra.gonde1@gmail.com>
2026-05-19 16:06:27 +0530

    Integrate Smoothiefroot API for watermelon data
    Add API call to fetch watermelon fruit data

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,6 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered! {name_on_smoothie}', icon="✅")
     
     
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
