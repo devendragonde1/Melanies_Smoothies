ee79ac976ff1ebc3e3586b0852ed9f80f1231cf3
devendragonde1 <devendra.gonde1@gmail.com>
2026-05-19 16:54:46 +0530

    Update dataframe display to use pandas
    Comment out the original dataframe display and convert the session table to a pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,10 @@ session = cnx.session()
 name_on_smoothie=st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ',name_on_smoothie)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 ingredients_list=st.multiselect(
 'Choose upto 5 ingredients:', my_dataframe, max_selections=5
