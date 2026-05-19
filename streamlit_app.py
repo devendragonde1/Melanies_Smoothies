c25d05676622fdaf626c21c273d620a71c969c4e
devendragonde1 <devendra.gonde1@gmail.com>
2026-05-19 16:50:29 +0530

    Fix column name in fruit_options query

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 name_on_smoothie=st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ',name_on_smoothie)
-my_dataframe = session.table("smoothies.public.fruit_options").select((col('Fruit_name'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 ingredients_list=st.multiselect(
